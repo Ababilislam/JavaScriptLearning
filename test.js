7221656a0e2df538d4dcc3a4e9b3bc7391dbfa5d
ABABIL ISLAM UDOY <ababilislamudoy@gmail.com>
2023-12-27 18:29:21 +0600

    add array method

--- test.js
+++ test.js
@@ -1,5 +1,39 @@
-let text = `Apple, Banana, Kiwi`
-ttext=text.matchAll(/Apple/g);
-console.log(ttext)
+// let text = `Apple, Banana, Kiwi`
+// ttext=text.replace("Apple","Mango");
+// console.log(ttext)
 
-document.getElementById("demo").innerHTML=ttext
+const cars =["saab","volvo","BMW"]
+
+
+
+let carlen = cars.length
+
+// data = cars[cars.length-1]
+// text = "<ul>"
+// for(i=0; i<carlen; i++){
+//     // console.log(cars[i])
+//     text+="<li>"+cars[i]+"</li>"
+// }
+// text+="<ul>"
+cars.push("daka")
+function myfunction(data){
+    text+="<li>"+data+"</li>"
+}
+
+console.log(cars)
+// text = "<ul>"
+// cars.forEach(myfunction);
+// text+="<ul>"
+// console.log(typeof cars)
+// text=cars.join(" ")
+
+//
+// let a = cars.shift()
+// text=cars
+
+// dynamic add to array
+// cars[cars.length] = "bamama"
+// text = cars
+
+
+document.getElementById("demo").innerHTML= text
